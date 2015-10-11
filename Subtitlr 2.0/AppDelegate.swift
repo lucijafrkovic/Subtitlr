dd0b359844ca273be2db3c66bd446bf835c53730
spilja <spilja@gmail.com>
2015-10-11 14:17:39 +0200

    Initial Commit

---
+++ Subtitlr 2.0/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Subtitlr 2.0
+//
+//  Created by Lucija Frković on 11/10/15.
+//  Copyright © 2015 Lucija Frković. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Subtitlr 2.0/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Subtitlr 2.0
+//
+//  Created by Lucija Frković on 11/10/15.
+//  Copyright © 2015 Lucija Frković. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
